8bf2cc6144eb434db39b61d32949345502a108be
UttamS <uttamsahoo6205@gmail.com>
2022-01-07 20:23:38 +0530

    my project

---
+++ src/main/java/com/example/AccessingDataJPA/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.example.AccessingDataJPA;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+}
---
+++ src/test/java/com/example/AccessingDataJPA/AccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccessingDataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
